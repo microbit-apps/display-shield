eb001646715fc5cb609830bcecedf12efcba17eb
KierPalin <45743174+KierPalin@users.noreply.github.com>
2025-04-22 16:52:04 +0100

    working state, cleanup comments

--- cpp/config_nrf.h
+++ cpp/config_nrf.h
@@ -17,19 +17,14 @@
 #define MY_PIN_BTNMX_CLOCK ((CODAL_PIN *)NULL)
 #define MY_PIN_BTNMX_DATA ((CODAL_PIN *)NULL)
 
+
+// Buttons:
 #define MY_PIN_WDS_TOP_BTN &uBit.io.P1   // Pin 31 DAL.P0.00
 #define MY_PIN_WDS_BOT_BTN &uBit.io.P2   // Pin 4 DAL.P0.04
 #define MY_PIN_WDS_LEFT_BTN &uBit.io.P21  // Pin 2 DAL.P0.00  (used by speaker)
 #define MY_PIN_WDS_RIGHT_BTN &uBit.io.P22 // Pin 17 DAL.P0.20 (used by runmic)
 
-
-// #define MY_PIN_DISPLAY_SCK &uBit.io.P13     // DAL.P0_17
-// #define MY_PIN_DISPLAY_SCK &uBit.io.P12     // DAL.P0_17
-// #define MY_PIN_DISPLAY_MOSI &uBit.io.P15    // DAL.P0_13
-// // #define MY_PIN_DISPLAY_MOSI &uBit.io.P13    // DAL.P0_13
-// #define MY_PIN_DISPLAY_DC &uBit.io.P8       // DAL.P0_10
-// #define MY_PIN_DISPLAY_RST &uBit.io.P16     // DAL.P1_2
-
+// Screen:
 #define MY_PIN_DISPLAY_SCK &uBit.io.P0  // DAL.P0_02
 #define MY_PIN_DISPLAY_MOSI &uBit.io.P4 // DAL.P0.28
 #define MY_PIN_DISPLAY_RST &uBit.io.P10 // DAL.P1_00
@@ -39,6 +34,8 @@
 #define MY_PIN_DISPLAY_MISO ((CODAL_PIN *)NULL) // not connected
 #define MY_PIN_DISPLAY_CS ((CODAL_PIN *)NULL)   // not connected
 #define MY_PIN_LED ((CODAL_PIN *)NULL)          // not connected
+
+
 
 #undef DEV_NUM_PINS
 #define DEV_NUM_PINS 48
