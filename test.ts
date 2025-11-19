f4afc829e474e8c856b25e3b5e7c90d01f0b0855
Thomas Ball <thomas.j.ball@outlook.com>
2025-11-19 10:52:48 +0000

    add screen painting

--- test.ts
+++ test.ts
@@ -1,15 +1,15 @@
-// controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-//     screen().fill(8)
-//     screen().drawLine(0, 0, 160, 120, 1)
-//     screen().drawLine(160, 0, 0, 120, 1)
-// })
-// controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-//     screen().fill(0)
-//     screen().fillCircle(0, 0, 20, 3)
-//     screen().fillCircle(160, 0, 20, 9)
-//     screen().fillCircle(160, 120, 20, 7)
-//     screen().fillCircle(0, 120, 20, 5)
-// })
+controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    screen().fill(8)
+    screen().drawLine(0, 0, 160, 120, 1)
+    screen().drawLine(160, 0, 0, 120, 1)
+})
+controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
+    screen().fill(0)
+    screen().fillCircle(0, 0, 20, 3)
+    screen().fillCircle(160, 0, 20, 9)
+    screen().fillCircle(160, 120, 20, 7)
+    screen().fillCircle(0, 120, 20, 5)
+})
 
 let presses = 0;
 controller.A.onEvent(
